068f49e03c137ed91a7a31c3bd4dc947d430f7d4
Maximilian Milz <maximilianmilz82@gmail.com>
2021-11-19 08:55:19 +0100

    Revised test class

--- src/test/java/de/thb/ics/IcsApplicationTests.java
+++ src/test/java/de/thb/ics/IcsApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IcsApplicationTests {
+public class IcsApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }

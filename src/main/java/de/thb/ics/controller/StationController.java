afb0c98800cd2fcf66a10fedbfe84aacc6b1868a
Jan Rueckert <jan.nr@live.de>
2021-11-22 17:09:10 +0100

    Add @CrossOrigin in order to access data from localhost

--- src/main/java/de/thb/ics/controller/StationController.java
+++ src/main/java/de/thb/ics/controller/StationController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/stations/")
 @RequiredArgsConstructor

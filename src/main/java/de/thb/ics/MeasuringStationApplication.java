92307f0d1ea2aa34718b1c568c236c5be1402c9f
Maximilian Milz <maximilianmilz82@gmail.com>
2021-11-25 14:26:27 +0100

    No jooq logo

--- src/main/java/de/thb/ics/MeasuringStationApplication.java
+++ src/main/java/de/thb/ics/MeasuringStationApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableScheduling
 public class MeasuringStationApplication {
@@ -12,4 +14,8 @@ public class MeasuringStationApplication {
         SpringApplication.run(MeasuringStationApplication.class, args);
     }
 
+    @PostConstruct
+    public void init() {
+        System.setProperty("org.jooq.no-logo", "true");
+    }
 }

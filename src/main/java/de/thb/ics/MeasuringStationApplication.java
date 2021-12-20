f800dae4691502836df7f427126a3874cc060b84
Maximilian Milz <maximilianmilz82@gmail.com>
2021-12-20 13:52:46 +0100

    Replaced postgres db with mysql db

--- src/main/java/de/thb/ics/MeasuringStationApplication.java
+++ src/main/java/de/thb/ics/MeasuringStationApplication.java
@@ -2,12 +2,10 @@ package de.thb.ics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-@EnableScheduling
 public class MeasuringStationApplication {
 
     public static void main(String[] args) {

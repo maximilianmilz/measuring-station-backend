899704b321411e36ce6978a7ef7e4253fb02cd9e
Maximilian Milz <maximilianmilz82@gmail.com>
2022-01-03 15:47:54 +0100

    allow all origins

--- src/main/java/de/thb/ics/controller/StationController.java
+++ src/main/java/de/thb/ics/controller/StationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/stations/")
 @RequiredArgsConstructor
